97376a32261815372089f3d3ca3f7f620168c76d
Alex <akp42@duke.edu>
2023-06-28 19:42:07 -0400

    set up template page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,17 @@
+
+import { Text } from "react-native";
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    text: {
+      fontSize: 24, // Change the font size as per your requirement
+      padding: 100
+    },
+  });
+
+export default function LandingPage(){
+    return(<>
+    <Text style={styles.text}>heyyyyyyyyyyy</Text>
+    
+    </>);
+}
--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -5,6 +5,8 @@ import { AuthContext } from "../provider/AuthProvider";
 import { NativeBaseProvider } from "native-base";
 import { supabase } from "../supabase";
 
+
+
 // IMPORT MAIN SCREENS HERE
 
 // Auth screens
@@ -14,6 +16,9 @@ import Signup from "../screens/auth/Signup";
 // put supabase stuff here?
 import Home from "../screens/main/Home";
 import ProfileCreator from "../screens/main/ProfileCreator";
+
+//Import Components
+import LandingPage from "../components/LandingPage";
 
 
 const AuthStack = createNativeStackNavigator();
@@ -77,11 +82,12 @@ export default () => {
     return (
       <NativeBaseProvider>
         <NavigationContainer>
-          {session === null || session.user === null || firstName === null ? (
+          <LandingPage />
+          {/* {session === null || session.user === null || firstName === null ? (
             <Auth />
           ) : (
             <Main />
-          )}
+          )} */}
         </NavigationContainer>
       </NativeBaseProvider>
     );
