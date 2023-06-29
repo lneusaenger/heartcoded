3cdc651d9ca348e4d97d08d6ce2a60dbea9366c2
Alex <akp42@duke.edu>
2023-06-29 11:36:27 -0400

    if i'm being honest i dont even remember what i did

---
+++ HeartcodedDev/HeartcodedDev/HeartcodedDevApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartcodedDevApp.swift
+//  HeartcodedDev
+//
+//  Created by Alex Pieroni on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct HeartcodedDevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
