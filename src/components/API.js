a35e2f6f2ba246586ce1ebf9eece21dd0d13a569
Alex <akp42@duke.edu>
2023-06-28 20:24:19 -0400

    working 🕺

--- src/components/API.js
+++ src/components/API.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 
+const API_URL = "https://api-inference.huggingface.co/models/gpt2";
+const API_TOKEN = "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"; // Replace with your actual Hugging Face API token
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -19,17 +22,45 @@ const styles = StyleSheet.create({
     padding: 8,
     marginBottom: 16,
   },
+  responseText: {
+    marginTop: 20,
+    fontSize: 16,
+    textAlign: "center",
+  },
 });
 
 export default function API() {
+    //input text
   const [inputText, setInputText] = useState("");
+
+  //api reply
+  const [apiResponse, setApiResponse] = useState(null);
 
   const handleInputChange = (text) => {
     setInputText(text);
   };
 
-  const handleApiCall = () => {
-    console.log("Here is inputText:", inputText);
+  const handleApiCall = async () => {
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${API_TOKEN}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          inputs: inputText,
+        }),
+      });
+
+      const data = await response.json();
+      const generatedText = data[0]?.generated_text || "";
+      setApiResponse(generatedText);
+      
+      console.log("API response:", data);
+    } catch (error) {
+      console.error("API call failed:", error);
+    }
   };
 
   return (
@@ -43,6 +74,11 @@ export default function API() {
         value={inputText}
       />
       <Button title="Submit" onPress={handleApiCall} />
+      {apiResponse && (
+        <Text style={styles.responseText}>
+          API response: {JSON.stringify(apiResponse)}
+        </Text>
+      )}
     </View>
   );
 }
